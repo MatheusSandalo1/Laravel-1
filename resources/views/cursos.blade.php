18f3d88611eb14ac6859da33f81fbeef5de4346b
MATHEUS SANDALO <matheus.sandalo5@gmail.com>
2023-01-20 20:34:56 -0300

    Initial commit

---
+++ resources/views/cursos.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cursos Senac Marília</title>
+</head>
+<body>
+    <h1>Cursos Senac Marília</h1>
+    <li>informática</li>
+    <li>Farmácia</li>
+    <li>Enfermagem</li>
+</body>
+</html>
